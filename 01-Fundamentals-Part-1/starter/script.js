8c03e8625021df14974ffb193df4d541f4f718a0
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-06-27 18:07:24 +0100

    Finished let, const and var section

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+// Values and Variables
+
 // let js = 'amazing';
 // console.log(40 + 8 + 23 - 10);
 
@@ -8,20 +10,37 @@
 // let javascriptIsFun = true;
 // console.log(javascriptIsFun)
 
+// Data Types 
+
 // console.log(typeof true);
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof 'Courtney');
 
-javascriptIsFun = 'YES!';
-console.log(javascriptIsFun)
+// javascriptIsFun = 'YES!';
+// console.log(javascriptIsFun)
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(year);
-console.log(typeof year);
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// Let, Const and Var
+
+// You can change a let variable
+let age = 30;
+age = 31;
+
+// You can't change a const variable
+const birthYear = 1994;
+// birthYear = 1991 <-- You can't do this
+
+// Old practice that people do not use anymore. Work similar to let
+var job = 'programmer'
+job = 'teacher'
+console.log(job);
