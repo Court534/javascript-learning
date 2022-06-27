de112c9b9c46cdb9b80038dadaaa15c1dfbacd99
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-06-27 17:51:37 +0100

    Finished the data types in Javascript section

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,27 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Courtney');
-let firstName = 'Courtney';
-console.log(firstName)
+// console.log('Courtney');
+// let firstName = 'Courtney';
+// console.log(firstName)
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Courtney');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
