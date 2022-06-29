602155a7076e6774f2044cbfb66bc7b8df30b833
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-06-29 19:19:27 +0100

    First sections completed on if else statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -104,8 +104,26 @@
 // lines`);
 
 
+// Taking Decisions: if / else Statements
 
+const age = 15;
 
+if(age >= 18) {
+  console.log('Sarah is old enough to drink on the UK :)')
+} else {
+  const yearsLeft = 18 - age
+  console.log(`Sorry you still have ${yearsLeft} years before you can drink :(`)
+};
+
+const birthYear = 2012;
+
+let century;
+if(birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21
+};
+console.log(century)
 
 
 
